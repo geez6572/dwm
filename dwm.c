95fac11dd2e758ffbd2f81f763c6be57f36f1507
ce <3127196570@qq.com>
2024-05-23 19:13:32 +0800

    add script dir

--- dwm.c
+++ dwm.c
@@ -1759,8 +1759,8 @@ run(void)
 
 void
 runAutostart(void) {
-	system("cd ~/scripts; ./autostart_blocking.sh");
-	system("cd ~/scripts; ./autostart.sh &");
+	system("cd /home/ce/soft/dwm/scripts; ./autostart_blocking.sh");
+	system("cd /home/ce/soft/dwm/scripts; ./autostart.sh &");
 }
 
 void
